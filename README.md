63877776721f34f606296f85f92963cd47baf45f
Pedro Mendes <me@altart.top>
2024-05-25 02:40:51 -0300

    feat: Initial boxed blur implementation

---
+++ README.md
@@ -0,0 +1,21 @@
+# [Box blur](https://en.wikipedia.org/wiki/Box_blur) implementation.
+
+This is not the fastest implementation and it's not intended to be. It's intended to be a simple implementation that is easy to understand made for me to study image processing.
+
+This implementation can replicate the results of the gaussian blur implementation of the image library that I'm using. The difference is that the gaussian blur implementation is faster and the box blur implementation is simpler and slower.
+
+## Usage
+
+```
+cargo run -- <path> <iterations>
+```
+
+Where `<path>` is the path to the image file and `<iterations>` is the number of iterations that the box blur algorithm will run.
+
+## Image example
+
+Original image:
+![Original image](./example.jpg)
+
+After 30 iterations:
+![Blurred image](./example_blurred.jpg)
---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use image::{io::Reader as ImageReader, ImageBuffer, Pixel, Rgb, Rgb32FImage, RgbImage};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn safe_get_pixel(img: &ImageBuffer<Rgb<f32>, Vec<f32>>, x: i32, y: i32) -> Rgb<f32> {
+    let width = img.width();
+    let height = img.height();
+    *img.get_pixel(
+        x.clamp(0, width as i32 - 1) as u32,
+        y.clamp(0, height as i32 - 1) as u32,
+    )
+}
+
+fn save_image(img: ImageBuffer<Rgb<f32>, Vec<f32>>, path: &str) -> Result<()> {
+    let img = RgbImage::from_fn(img.width(), img.height(), |x, y| {
+        let pixel = img.get_pixel(x, y);
+        Rgb([
+            (pixel[0] * 255.0).round() as u8,
+            (pixel[1] * 255.0).round() as u8,
+            (pixel[2] * 255.0).round() as u8,
+        ])
+    });
+    println!("Saving image to {}", path);
+    img.save(path)?;
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <image_path> <iterations>", args[0]);
+        std::process::exit(1);
+    }
+
+    let img_path = &args[1];
+    let mut iterations = args[2]
+        .parse::<i32>()
+        .expect("Iterations must be an integer");
+
+    if iterations % 2 == 1 {
+        iterations += 1;
+    }
+    println!("Blurring image {} with {} iterations", img_path, iterations);
+    let img = ImageReader::open(img_path).unwrap().decode().unwrap();
+    let mut img_buf = img.to_rgb32f();
+    let mut new_img = Rgb32FImage::new(img.width(), img.height());
+    for _ in 0..iterations {
+        for y in 0..img.height() {
+            for x in 0..img.width() {
+                let mut r = 0.0;
+                let mut g = 0.0;
+                let mut b = 0.0;
+                for i in -1..=1 {
+                    for j in -1..=1 {
+                        let pixel = safe_get_pixel(&img_buf, x as i32 + i, y as i32 + j);
+                        r += pixel[0] * pixel[0];
+                        g += pixel[1] * pixel[1];
+                        b += pixel[2] * pixel[2];
+                    }
+                }
+                let average_r = (r / 9.0).sqrt();
+                let average_g = (g / 9.0).sqrt();
+                let average_b = (b / 9.0).sqrt();
+                new_img.put_pixel(x, y, Rgb::<f32>([average_b, average_g, average_r]));
+            }
+        }
+        img_buf = ImageBuffer::from_fn(img.width(), img.height(), |x, y| {
+            new_img.get_pixel(x, y).to_rgb()
+        });
+    }
+
+    let file_name = img_path
+        .split('/')
+        .last()
+        .unwrap()
+        .split('.')
+        .next()
+        .unwrap();
+
+    save_image(new_img, format!("{}_blurred.jpg", file_name).as_str())?;
+
+    Ok(())
+}
